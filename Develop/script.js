3a20baefa75342d3e10ebcadb168ab7d076dcc1a
Jeff Small <jeffmeister1990@gmail.com>
2022-06-05 23:44:50 -0400

    Edited current day information

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,5 @@
 // Display current day
-var currentDay = moment().format("dddd, MMMM do, YYYY");
+var currentDay = moment().format("dddd, MMMM Do, YYYY");
 $('#currentDay').append(currentDay);
 
 // Time block color-coded to indicate whether it is in the past, present, or future
-
-// Clicking into a timeblock allows you to enter an event
-
-// Clicking save stores the event in local storage
-
-// When refreshing the page the saved events persist
